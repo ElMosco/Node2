f33900c044aec4f217d7c64a587053bfac7557c3
Hansith De Silva <de.silva.hansith.lakdinu@iisgalvanimi.edu.it>
2020-05-08 08:52:49 +0000

    punto 03.2

--- relab-server/index.js
+++ relab-server/index.js
@@ -3,40 +3,19 @@ const app = new express();
 const sql = require('mssql');
 const cors = require('cors');
 const CC = require('./CoordConverter.js');
+const sqlUtils = require ('./SqlUtils.js');
 
 app.use(new cors());
 
-const coordConverter =  new CC();
-
-const config = {
-    user: 'PCTO',  //Vostro user name
-    password: 'xxx123#', //Vostra password
-    server: "213.140.22.237",  //Stringa di connessione
-    database: 'Katmai', //(Nome del DB)
-}
-
-app.get('/', function (req, res) {
-    //res.send('Hello World!');
-    sql.connect(config, (err) => {
-        if (err) console.log(err);  // ... error check
-        else makeSqlRequest(res);     // 
-    });
+app.get('/', function(req, res){
+    sqlUtils.connect(req, res,sqlUtils.makeSqlRequest);
 });
 
-function makeSqlRequest(res) {
-    let sqlRequest = new sql.Request();  //sqlRequest: oggetto che serve a eseguire le query
-    let q = 'SELECT DISTINCT TOP (100) [GEOM].STAsText() FROM [Katmai].[dbo].[interventiMilano]';
-    //eseguo la query e aspetto il risultato nella callback
-    sqlRequest.query(q, (err, result) => {sendQueryResults(err,result,res)}); 
-}
-
-
-function sendQueryResults(err,result, res)
-{
-    if (err) console.log(err); // ... error checks
-    res.send(coordConverter.generateGeoJson(result.recordset));  //Invio il risultato al Browser
-}
-
+app.get('/ci_vettore/:foglio', function (req, res) {
+    console.log(req.params.foglio);
+    //richiamo il metodo che ottiene l'elenco dei vettori energetici
+    sqlUtils.connect(req, res, sqlUtils.ciVettRequest);
+ });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
